c0564cfa698f71c7f25261e8aaa6c78d61850bca
dljvandenberg <dljvandenberg@gmail.com>
2015-12-06 14:17:46 +0100

    Move TODOs to new file backlog.md

--- R/predict_maintenance.R
+++ R/predict_maintenance.R
@@ -24,9 +24,6 @@ df.train.values <- read.csv("./data/training_set_values.csv", na.strings=c(""),
 df.test <- read.csv("./data/test_set_values.csv", na.strings=c(""),
                            colClasses=c("factor", "numeric", "POSIXct", "factor", "integer", "factor", "numeric", "numeric", "factor", "integer", "factor", "factor", "factor", "factor", "factor", "factor", "factor", "integer", "factor", "factor", "factor", "factor", "factor", "integer", "factor", "factor", "factor", "factor", "factor", "factor", "factor", "factor", "factor", "factor", "factor", "factor", "factor", "factor", "factor", "factor"))
 
-# TODO_later: check assumptions about 0 values (we kept these 0 for the moment in order to prevent issues during ML)
-# TODO_later: check long/latitude for zero's
-
 # Replace 0 by NA for certain variables (based on our assumption that these 0 values are incorrect..)
 #df.train.values$construction_year[df.train.values$construction_year==0] <- NA
 #df.train.values$gps_height[df.train.values$gps_height==0] <- NA
@@ -39,51 +36,51 @@ df.test <- read.csv("./data/test_set_values.csv", na.strings=c(""),
 #df.test$amount_tsh[df.test$amount_tsh==0] <- NA
 #df.test$population[df.test$population==0] <- NA
 
-# Note: there are many pumps with wpt_name=="none". We left these unchanged.
-
 # Merge training values and labels and remove obsolete data frames
-df.train <- merge(df.train.values, df.train.labels, by="id")
+df.train.raw <- merge(df.train.values, df.train.labels, by="id")
 rm(df.train.values, df.train.labels)
-
-# Remove near zero variance variables ("gps_height"  "num_private" "recorded_by")
-list.nzv <- nearZeroVar(df.train)
-df.train <- df.train[-list.nzv]
-
-# Drop id variable before applying machine learning
-df.train <- subset(df.train, select=-id)
 
 
 
 ## EXPLORE
 
+# Check data quality (NAs, unknowns, zeros)
+summary(df.train.raw)
+m.train.naratios <- as.matrix(colSums(is.na(df.train.raw))/(dim(df.train.raw)[1]))
+plot(m.train.naratios)
+
 # Count status_group categories
-table(df.train$status_group)
+table(df.train.raw$status_group)
 
 # Exploratory plots
-#with(df.train, table(status_group, region))
-ggplot(df.train, aes(x=region, fill=status_group)) + geom_bar()
-ggplot(df.train, aes(x=source_type, fill=status_group)) + geom_bar()
-ggplot(df.train, aes(x=construction_year, fill=status_group)) + geom_bar()
-ggplot(df.train, aes(x=construction_year, fill=status_group)) + geom_bar(position="fill") + ylab("ratio")
-ggplot(df.train, aes(x=waterpoint_type, fill=status_group)) + geom_bar(position="fill") + ylab("ratio")
-ggplot(df.train, aes(x=extraction_type_group, fill=status_group)) + geom_bar(position="fill") + ylab("ratio")
-ggplot(df.train, aes(x=waterpoint_type, fill=status_group)) + geom_bar(position="fill") + ylab("ratio")
+#with(df.train.raw, table(status_group, region))
+ggplot(df.train.raw, aes(x=region, fill=status_group)) + geom_bar()
+ggplot(df.train.raw, aes(x=source_type, fill=status_group)) + geom_bar()
+ggplot(df.train.raw, aes(x=construction_year, fill=status_group)) + geom_bar()
+ggplot(df.train.raw, aes(x=construction_year, fill=status_group)) + geom_bar(position="fill") + ylab("ratio")
+ggplot(df.train.raw, aes(x=waterpoint_type, fill=status_group)) + geom_bar(position="fill") + ylab("ratio")
+ggplot(df.train.raw, aes(x=extraction_type_group, fill=status_group)) + geom_bar(position="fill") + ylab("ratio")
+ggplot(df.train.raw, aes(x=waterpoint_type, fill=status_group)) + geom_bar(position="fill") + ylab("ratio")
+
+
+
+## ADDITIONAL CLEANING
+
+# Remove near zero variance variables ("gps_height"  "num_private" "recorded_by")
+list.nzv <- nearZeroVar(df.train.raw)
+df.train.select <- df.train.raw[-list.nzv]
+
+# Drop id variable before applying machine learning
+df.train.select <- subset(df.train.select, select=-id)
 
 
 ## MACHINE LEARNING
 
-# NOTE: rpart model with predictor subvillage requires 2.8Gb, wpt_name also requires a lot
-# TODO: troubleshoot error when using funder, scheme_name as predictors
-# TODO_later: use p=.75
-# TODO_later: rename df.training (to prevent confusion with df.train)
-# TODO_later: drop wpt_name?, date_recorded?
-# TODO_later: remove df.train to clear up memory
-
 # Divide into training, testing and predicting set
 set.seed(1234)
-m.train <- createDataPartition(df.train$status_group, p=.1, list = FALSE)
-df.training <- df.train[m.train,]
-df.validating <- df.train[-m.train,]
+m.train <- createDataPartition(df.train.select$status_group, p=.1, list = FALSE)
+df.training <- df.train.select[m.train,]
+df.validating <- df.train.select[-m.train,]
 
 # Remove near zero variance variables ("gps_height"  "num_private" "recorded_by")
 list.nzv <- nearZeroVar(df.training)
@@ -105,8 +102,6 @@ model.rpart.1 <- train(factor(status_group) ~ region + quantity, data=df.trainin
 model.rf.1 <- train(factor(status_group) ~ region + quantity, data=df.training, method="rf", preProc="knnImpute")
 #model.rf.2 <- train(factor(status_group) ~ construction_year + public_meeting + scheme_management + permit + extraction_type + management + payment + water_quality + quantity + source + waterpoint_type, data=df.training, method="rf")
 #model.rf.3 <- train(factor(status_group) ~ amount_tsh + date_recorded + installer + longitude + latitude + basin + region + region_code + district_code + lga + ward + population + public_meeting + scheme_management + permit + construction_year + extraction_type + extraction_type_group + extraction_type_class + management + management_group + payment + payment_type + water_quality + quality_group + quantity + quantity_group + source + source_type + source_class + waterpoint_type + waterpoint_type_group, data=df.training, method="rf")
-#model.rf.4 <- train(factor(status_group) ~ ., data=df.training, method="rf", trControl=trainControl(method="cv", number=10))
-#model.gbm <- train(factor(status_group) ~ ., data=df.training, method="gbm")
 
 # Select model
 model.selected <- model.rf.1
---
+++ backlog.md
@@ -0,0 +1,38 @@
+# Backlog
+
+#. Submit first correct results
+#. Increase data quality:
+    a. Check ratio of NAs, unknowns, zeros, wpt_name=="none", near zeros in long/latitude, date_recorded (and document reasons to exclude)
+    a. Replace incorrect zeros by NAs? (and write down assumptions)
+    a. Test imputation of missing values (during preProc="knnImpute"; also test on validation/test sets)
+#. Troubleshoot memory issues
+    a. Read more info
+    a. Find limitations
+    a. Don't use subvillage and wpt_name as predictors (too much memory usage)
+    a. Remove unused dataframes to clear up memory (df.train.raw)?
+#. Refactoring: put code in functions
+#. Use p=.75 in splitting training/validating sets
+#. Train more complicated models
+    a. Include more predictors
+    a. method="gbm"
+    a. trControl=trainControl(method="cv", number=10)
+
+
+# Notes
+
+* Variables with nonzero NA ratio:
+
+```
+ scheme_name           0.474175084
+ scheme_management     0.065269360
+ installer             0.061531987
+ funder                0.061195286
+ public_meeting        0.056127946
+ permit                0.051447811
+ subvillage            0.006245791
+```
+
+
+# Assumptions
+
+* ..
