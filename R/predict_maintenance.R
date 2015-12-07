580edcc75824bdd8446ecb9d2930ddea9f1dc2cf
dljvandenberg <dljvandenberg@gmail.com>
2015-12-07 13:08:57 +0100

    Refactoring (variable names) + extend rf model

--- R/predict_maintenance.R
+++ R/predict_maintenance.R
@@ -1,5 +1,7 @@
 ### Predict Water Pump Maintenance
 
+# Convention: prefix of variable names denotes type (l.* = list, m.* = matrix, df.* = dataframe, model.* = model)
+
 
 ## PREPARE
 
@@ -59,15 +61,15 @@ ggplot(df.train.raw, aes(x=source_type, fill=status_group)) + geom_bar()
 ggplot(df.train.raw, aes(x=construction_year, fill=status_group)) + geom_bar()
 ggplot(df.train.raw, aes(x=construction_year, fill=status_group)) + geom_bar(position="fill") + ylab("ratio")
 ggplot(df.train.raw, aes(x=waterpoint_type, fill=status_group)) + geom_bar(position="fill") + ylab("ratio")
-ggplot(df.train.raw, aes(x=payment, fill=status_group)) + geom_bar(position="fill") + ylab("ratio")
+ggplot(df.train.raw, aes(x=management, fill=status_group)) + geom_bar(position="fill") + ylab("ratio")
 
 
 
 ## ADDITIONAL CLEANING
 
 # Remove near zero variance variables ("gps_height"  "num_private" "recorded_by")
-list.nzv <- nearZeroVar(df.train.raw)
-df.train.select <- df.train.raw[-list.nzv]
+l.nzv <- nearZeroVar(df.train.raw)
+df.train.select <- df.train.raw[-l.nzv]
 
 # Drop id variable before applying machine learning
 df.train.select <- subset(df.train.select, select=-id)
@@ -82,15 +84,15 @@ df.training <- df.train.select[m.train,]
 df.validating <- df.train.select[-m.train,]
 
 # Remove near zero variance variables ("gps_height"  "num_private" "recorded_by")
-list.nzv <- nearZeroVar(df.training)
-if(length(list.nzv) > 0) {
-    print(paste("Removing columns with near zero variance: ", list.nzv))
-    df.training <- df.training[-list.nzv]
+l.nzv <- nearZeroVar(df.training)
+if(length(l.nzv) > 0) {
+    print(paste("Removing columns with near zero variance: ", l.nzv))
+    df.training <- df.training[-l.nzv]
 }
 
 # Train
 set.seed(2345)
-model.rpart.1 <- train(factor(status_group) ~ region + quantity, data=df.training, method="rpart", preProc="knnImpute")
+#model.rpart.1 <- train(factor(status_group) ~ region + quantity, data=df.training, method="rpart", preProc="knnImpute")
 
 # model.rpart.2 <- train(factor(status_group) ~ construction_year + public_meeting + scheme_management + permit + extraction_type + management + payment + water_quality + quantity + source + waterpoint_type,
 #                         data=df.training,
@@ -98,28 +100,29 @@ model.rpart.1 <- train(factor(status_group) ~ region + quantity, data=df.trainin
 #                         preProc="knnImpute"
 #                         )
 
-model.rf.1 <- train(factor(status_group) ~ region + quantity, data=df.training, method="rf", preProc="knnImpute")
-model.rf.2 <- train(factor(status_group) ~ region + quantity + waterpoint_type + payment, data=df.training, method="rf", preProc="knnImpute")
-#model.rf.2 <- train(factor(status_group) ~ construction_year + public_meeting + scheme_management + permit + extraction_type + management + payment + water_quality + quantity + source + waterpoint_type, data=df.training, method="rf")
+#model.rf.1 <- train(factor(status_group) ~ region + quantity, data=df.training, method="rf", preProc="knnImpute")
+#model.rf.2 <- train(factor(status_group) ~ region + quantity + waterpoint_type + payment, data=df.training, method="rf", preProc="knnImpute")
+model.rf.3 <- train(factor(status_group) ~ region + quantity + waterpoint_type + payment + extraction_type + management + water_quality + source, data=df.training, method="rf", preProc="knnImpute")
+#model.rf.2 <- train(factor(status_group) ~ construction_year + extraction_type + management + payment + water_quality + quantity + source + waterpoint_type, data=df.training, method="rf")
 #model.rf.3 <- train(factor(status_group) ~ amount_tsh + date_recorded + installer + longitude + latitude + basin + region + region_code + district_code + lga + ward + population + public_meeting + scheme_management + permit + construction_year + extraction_type + extraction_type_group + extraction_type_class + management + management_group + payment + payment_type + water_quality + quality_group + quantity + quantity_group + source + source_type + source_class + waterpoint_type + waterpoint_type_group, data=df.training, method="rf")
 
 # Select model
-model.selected <- model.rf.2
+model.selected <- model.rf.3
 
 # Model details
 model.selected
 model.selected$finalModel
 
 # Confusion table and accuracy for validating set
-list.validating.predictions <- predict(model.selected, newdata=df.validating, na.action=na.fail)
-table(df.validating$status_group, list.validating.predictions)
-sum(df.validating$status_group == list.validating.predictions) / length(df.validating$status_group)
+l.validating.predictions <- predict(model.selected, newdata=df.validating, na.action=na.fail)
+table(df.validating$status_group, l.validating.predictions)
+sum(df.validating$status_group == l.validating.predictions) / length(df.validating$status_group)
 
 # Most important variables
 varImp(model.selected)
 
 # Save/load model to/from file
-saveRDS(model.selected, "./models/model.rf.2_p02_region_quantity_waterpointtype_payment.rds")
+saveRDS(model.selected, "./models/model.rf.3_p02_region_quantity_waterpointtype_payment_extractiontype_management_waterquality_source.rds")
 
 
 
@@ -128,9 +131,9 @@ saveRDS(model.selected, "./models/model.rf.2_p02_region_quantity_waterpointtype_
 #model.selected <- readRDS("./models/model.rf.1_p01_region_quantity.rds")
 
 # Apply model to test set
-list.test.predictions <- predict(model.selected, newdata=df.test, na.action=na.fail)
+l.test.predictions <- predict(model.selected, newdata=df.test, na.action=na.fail)
 
 # Save results as csv with variables id, status_group
-df.predictions <- transform(df.test, status_group=list.test.predictions)
+df.predictions <- transform(df.test, status_group=l.test.predictions)
 df.predictions <- subset(df.predictions, select=c("id", "status_group"))
-write.csv(df.predictions, "./predictions/predictions.rf.2_p02_region_quantity_waterpointtype_payment.csv", row.names=FALSE)
+write.csv(df.predictions, "./predictions/predictions.rf.3_p02_region_quantity_waterpointtype_payment_extractiontype_management_waterquality_source.csv", row.names=FALSE)
